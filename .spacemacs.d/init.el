8b73914473cb020085542c39028c4468e531fa7b
LanternD <yangdavid1028@gmail.com>
2017-12-04 22:02:28 -0500

    add progress bar. from 3110.

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -229,7 +229,7 @@ values."
    ;; If non nil a progress bar is displayed when spacemacs is loading. This
    ;; may increase the boot time on some systems and emacs builds, set it to
    ;; nil to boost the loading time. (default t)
-   dotspacemacs-loading-progress-bar nil
+   dotspacemacs-loading-progress-bar t
    ;; If non nil the frame is fullscreen when Emacs starts up. (default nil)
    ;; (Emacs 24.4+ only)
    dotspacemacs-fullscreen-at-startup nil
